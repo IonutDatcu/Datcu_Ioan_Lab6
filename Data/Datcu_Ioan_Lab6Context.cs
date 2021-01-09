0eee943b146694600d75d2d248cf67ebe0ddef26
IOAN- ADRIAN DATCU <ioan.datcu@stud.ubbcluj.ro>
2021-01-09 08:58:44 +0200

    Add project files.

---
+++ Data/Datcu_Ioan_Lab6Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Datcu_Ioan_Lab6.Models;
+
+namespace Datcu_Ioan_Lab6.Data
+{
+    public class Datcu_Ioan_Lab6Context : DbContext
+    {
+        public Datcu_Ioan_Lab6Context (DbContextOptions<Datcu_Ioan_Lab6Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Datcu_Ioan_Lab6.Models.Book> Book { get; set; }
+    }
+}
